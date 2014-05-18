809724f5ed3cbb84856d3f31ec184f582f731b75
Nasrallah <simon_nasrallah@hotmail.com>
2014-05-18 16:39:06 +0100

    initial commit

---
+++ test/test_calc.py
@@ -0,0 +1,15 @@
+import unittest
+from calc import Calculator
+
+
+
+class WorkflowTestCase(unittest.TestCase):
+
+    def test_adding_one_and_one_is_two(self):
+        calc = Calculator()
+        result = calc.add(1,1)
+        self.assertEqual(2, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
